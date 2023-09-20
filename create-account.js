6dd03d5e3183023a93dc55b6bb33b16b55a6ba6d
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-09-19 21:17:48 -0500

    solving methods

--- create-account.js
+++ create-account.js
@@ -1,5 +1,48 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin = "1234", amount = 0) {
+  const pinNumber = pin;
+  let bankAmount = amount;
+  return {
+    checkBalance: () => {
+      if (typeof pin === "number" && pin === pinNumber) {
+        return amount;
+      }
+      return "Invalid PIN";
+    },
+    deposit: () => {
+      const amount = checkBalance();
+      if (typeof amount === "number") {
+        bankAmount = bankAmount + amount;
+        return `Succesfully deposited $${amount}. Current balance: $${bankAmount}.`;
+      } else return "Invalid PIN";
+    },
+    withdraw: () => {
+      const amount = checkBalance();
+      if (typeof amount === "number") {
+        let newBalance = bankAmount - amount;
+        if (newBalance >= 0)
+          return `Succesfully withdrew  $${amount}. Current balance: $${bankAmount}.`;
+        else if (newBalance < 0)
+          return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      } else return "Invalid PIN";
+    },
+  };
 }
+
+let account = createAccount("1234", 100);
+
+account.checkBalance("oops");
+// "Invalid PIN."
+
+account.deposit("1234", 250);
+// "Succesfully deposited $250. Current balance: $350."
+
+account.withdraw("1234", 300);
+// "Succesfully withdrew $300. Current balance: $50."
+
+account.withdraw("1234", 10);
+// "Withdrawal amount exceeds account balance. Transaction cancelled."
+
+account.changePin("1234", "5678");
+// "PIN successfully changed!"
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,26 @@
 function guessingGame() {
-
+    const randomNum = Math.floor(Math.random() * 100)
+    let isWin = false
+    let count=1
+  return function(num) {
+    if (num > randomNum) {
+        count++
+        return `${num} is too high!`}
+    else if (num < randomNum)  {
+        count++
+        return `${num} is too low!`
+    }
+    else if (num === randomNum && isWin) return `The game is over, you already won!`
+    isWin = true
+    return `You win! You found ${randomNum} in ${count} guesses.`
+  }
+    
 }
+
+let game = guessingGame();
+console.log(game(50)); // "50 is too low!"
+console.log(game(90)); // "90 is too high!"
+console.log(game(70)); // "You win! You found 70 in 3 guesses."
+console.log(game(70)); // "The game is over, you already won!"
 
 module.exports = { guessingGame };
