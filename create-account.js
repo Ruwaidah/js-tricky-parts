91d53ddca6bd86dc8d674d7bc2d51a18a1872231
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-09-20 17:55:27 -0500

    solve all requirements

--- create-account.js
+++ create-account.js
@@ -1,28 +1,33 @@
 function createAccount(pin = "1234", amount = 0) {
-  const pinNumber = pin;
+  let pinNumber = pin;
   let bankAmount = amount;
   return {
-    checkBalance: () => {
-      if (typeof pin === "number" && pin === pinNumber) {
+    checkBalance: (p, a) => {
+      if (p === pinNumber) {
         return amount;
       }
       return "Invalid PIN";
     },
-    deposit: () => {
-      const amount = checkBalance();
-      if (typeof amount === "number") {
-        bankAmount = bankAmount + amount;
-        return `Succesfully deposited $${amount}. Current balance: $${bankAmount}.`;
+    deposit: (p, a) => {
+      if (p === pinNumber) {
+        bankAmount = bankAmount + a;
+        return `Succesfully deposited $${a}. Current balance: $${bankAmount}.`;
       } else return "Invalid PIN";
     },
-    withdraw: () => {
-      const amount = checkBalance();
-      if (typeof amount === "number") {
-        let newBalance = bankAmount - amount;
-        if (newBalance >= 0)
-          return `Succesfully withdrew  $${amount}. Current balance: $${bankAmount}.`;
-        else if (newBalance < 0)
+    withdraw: (p, a) => {
+      if (pinNumber === p) {
+        let newBalance = bankAmount - a;
+        if (newBalance >= 0) {
+          bankAmount = newBalance;
+          return `Succesfully withdrew  $${a}. Current balance: $${bankAmount}.`;
+        } else if (newBalance < 0)
           return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      } else return "Invalid PIN";
+    },
+    changePin: (oldPin, newPin) => {
+      if (pinNumber === oldPin) {
+        pinNumber = newPin;
+        return "PIN successfully changed!";
       } else return "Invalid PIN";
     },
   };
@@ -30,19 +35,21 @@ function createAccount(pin = "1234", amount = 0) {
 
 let account = createAccount("1234", 100);
 
-account.checkBalance("oops");
+console.log(account.checkBalance("oops"));
 // "Invalid PIN."
 
-account.deposit("1234", 250);
+console.log(account.deposit("1234", 250));
 // "Succesfully deposited $250. Current balance: $350."
 
-account.withdraw("1234", 300);
+console.log(account.withdraw("1234", 300));
 // "Succesfully withdrew $300. Current balance: $50."
 
-account.withdraw("1234", 10);
+console.log(account.withdraw("1234", 10));
 // "Withdrawal amount exceeds account balance. Transaction cancelled."
 
-account.changePin("1234", "5678");
+console.log(account.changePin("1234", "5678"));
 // "PIN successfully changed!"
+
+console.log(account.deposit("1234", 250));
 
 module.exports = { createAccount };
--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,17 @@
-function curriedAdd(total) {
-
+function curriedAdd(total = 0) {
+  return function addNext(num) {
+    if (!num) return total;
+    total += num;
+    return addNext;
+  };
 }
+
+let firstAdder = curriedAdd();
+let secondAdder = curriedAdd();
+let thirdAdder = curriedAdd();
+
+console.log(firstAdder()); // 0
+console.log(secondAdder(1)(2)()); // 3
+console.log(thirdAdder(2)(8)(5)(1)()); // 16
 
 module.exports = { curriedAdd };
